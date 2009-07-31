e621dd0f2091b3e2696da810bf49f059cce25919
John Hurst <john.b.hurst@gmail.com>
2009-08-01 11:00:48 +1200

    Added cropped screenshot images for wiki.

---
+++ bin/mkimages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function stdcrop() {
+  convert images/$1.png -crop 440x200+0+200 images/$1-cropped.png
+}
+
+stdcrop chrono-logical-grails-default-list
+stdcrop chrono-logical-grails-default-create
+
