8051beb2285ad462a86742b9b064e4f31a2125f1
John Hurst <john.b.hurst@gmail.com>
2009-08-02 14:36:08 +1200

    Added screen shot of js calendar plugin.

--- bin/mkimages.sh
+++ bin/mkimages.sh
@@ -4,9 +4,13 @@ function stdcrop() {
   convert images/$1.png -crop 440x200+0+200 images/$1-cropped.png
 }
 
+function bigcrop() {
+  convert images/$1.png -crop 520x300+0+200 images/$1-cropped.png
+}
+
 stdcrop chrono-logical-grails-default-list
 stdcrop chrono-logical-grails-default-create
 stdcrop chrono-logical-grails-default-create-precision-day
-stdcrop chrono-logical-grails-calendar-create
+bigcrop chrono-logical-grails-calendar-create
 
 
